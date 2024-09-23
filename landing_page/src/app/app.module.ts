d935c0d50068367f5096c38357caa2d41d84f3e9
Kevin <moralesperezkevin7@gmail.com>
2024-09-22 22:11:48 -0600

    add: Generamos el componente nav bar

--- landing_page/src/app/app.module.ts
+++ landing_page/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { DiscographyPageComponent } from './discography-page/discography-page.component';
 import { InfluencePageComponent } from './influence-page/influence-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     DiscographyPageComponent,
-    InfluencePageComponent
+    InfluencePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
