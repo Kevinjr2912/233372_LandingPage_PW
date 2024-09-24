49a7b6235a3af4c172ea4b6e7dce7b91be4f172d
Kevin <moralesperezkevin7@gmail.com>
2024-09-24 12:42:36 -0600

    add: enrutamiento

--- landing_page/src/app/app-routing.module.ts
+++ landing_page/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DiscographyPageComponent } from './discography-page/discography-page.component';
+import { InfluencePageComponent } from './influence-page/influence-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomePageComponent},
+  {path:"discography", component:DiscographyPageComponent},
+  {path:"influence", component:InfluencePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
