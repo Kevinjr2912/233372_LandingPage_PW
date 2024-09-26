860352848056daa264a586e7f76f3c98dd59bd79
Kevin <moralesperezkevin7@gmail.com>
2024-09-25 20:25:29 -0600

    add: detallando estilos

--- landing_page/src/app/app-routing.module.ts
+++ landing_page/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { DiscographyPageComponent } from './discography-page/discography-page.component';
