2719772851d93bd348871da13a58b82b622d4d7a
Kevin <moralesperezkevin7@gmail.com>
2024-10-14 12:40:29 -0600

    add: arreglando el mal uso de componentes

--- landing_page/src/app/influence-page/influence-page.component.ts
+++ landing_page/src/app/influence-page/influence-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMemberInfluence } from '../interfaces/imember-influence';
 
 @Component({
   selector: 'app-influence-page',
@@ -6,31 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './influence-page.component.css'
 })
 export class InfluencePageComponent {
-  artists = [
+  artists: IMemberInfluence[] = [
     {
       name: 'THE DOORS',
       content: 'Inspiración musical más fuerte, sobre todo en lo que hace a su forma de componer y proponer canciones. ',
-      img: 'assets/img/TheDoors.jpg'
+      urlImg: 'assets/img/TheDoors.jpg'
     },
     {
       name: 'PEARL JAM',
       content: 'Para el líder de The Strokes, escuchar el clásico track Yellow Ledbetter lo motivó a iniciarse en la música.',
-      img: 'assets/img/PearlJam.jpg'
+      urlImg: 'assets/img/PearlJam.jpg'
     },
     {
       name: 'SONIC YOUTH',
       content: 'Julian Casablancas comentó que para la cancion 12.51, su banda “tomó prestadas” ideas de la canción Bull in the Heather',
-      img: 'assets/img/SonicYouth.jpg'
+      urlImg: 'assets/img/SonicYouth.jpg'
     },
     {
       name: 'THE VELVET UNDERGROUND',
       content: 'Su influencia puede escucharse en los frenéticos estilos rítmicos motorik, especialmente marcados por la batería',
-      img: 'assets/img/TheVelvetUnderground.jpg'
+      urlImg: 'assets/img/TheVelvetUnderground.jpg'
     },
     {
       name: 'CYNDI LAUPER',
       content: 'Inspiración por su estética punk y sensibilidad pop tan características',
-      img: 'assets/img/CyndiLauper.jpg'
+      urlImg: 'assets/img/CyndiLauper.jpg'
     }
   ]
 }
---
+++ landing_page/src/app/interfaces/imember-influence.ts
@@ -0,0 +1,5 @@
+export interface IMemberInfluence {
+    name: string,
+    content: string,
+    urlImg: string
+}
